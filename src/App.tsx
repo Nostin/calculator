c2be7d64f65c8483986e22c9a1ebc9eb27f6832d
Sean Thompson <sean.thompson@publicissapient.com>
2021-02-19 09:37:37 +1100

    Architecture set up

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { CalculatorContainer } from './components/CalculatorContainer'
+
+const CalculatorApp: React.FC = () => {
+  return (
+    <div className="flex justify-center">
+      <CalculatorContainer />
+    </div>
+  )
+}
+
+export default CalculatorApp
---
+++ src/components/CalculatorContainer.tsx
@@ -0,0 +1,7 @@
+export const CalculatorContainer: React.FC = () => {
+  return (
+    <div className="w-80">
+      <p>calculator</p>
+    </div>
+  )
+}
